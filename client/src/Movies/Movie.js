fd59bda8c03e441a7a9ea4fe9b726144c30c0373
Lindsey C <lindseyacason@gmail.com>
2019-09-19 19:11:19 -0500

    forked

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Route } from 'react-router-dom'
 import axios from "axios";
 import MovieCard from "./MovieCard";
+import Form from "../Form"
+
 export default class Movie extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      movie: null
+      movie: "",
+      id:""
     };
   }
 
@@ -24,12 +28,17 @@ export default class Movie extends React.Component {
       .get(`http://localhost:5000/api/movies/${id}`)
       .then(res => this.setState({ movie: res.data }))
       .catch(err => console.log(err.response));
+      console.log(this.state.movie)
   };
+
+
+
 
   saveMovie = () => {
     const addToSavedList = this.props.addToSavedList;
     addToSavedList(this.state.movie);
   };
+
 
   render() {
     if (!this.state.movie) {
@@ -42,6 +51,13 @@ export default class Movie extends React.Component {
         <div className="save-button" onClick={this.saveMovie}>
           Save
         </div>
+       
+       {/* BUTTON GOES HERE */}
+        {/* <div className="update-button" onClick={"/update-movie/:id"}>Update</div> */}
+<Route path="/update-movie/:id" component={Form}>UPDATE</Route>
+
+
+    
       </div>
     );
   }
